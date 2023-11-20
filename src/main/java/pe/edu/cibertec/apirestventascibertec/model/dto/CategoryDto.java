65de4423b699a99ca3d659faf7cb476e443d6591
Airthon1234 <cipoteland@gmail.com>
2023-11-20 18:59:54 -0500

    avance

---
+++ src/main/java/pe/edu/cibertec/apirestventascibertec/model/dto/CategoryDto.java
@@ -0,0 +1,10 @@
+package pe.edu.cibertec.apirestventascibertec.model.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryDto implements DtoEntity{
+
+    private Integer categorid;
+    private String categoryname;
+}
---
+++ src/main/java/pe/edu/cibertec/apirestventascibertec/model/dto/SupplierDto.java
@@ -0,0 +1,10 @@
+package pe.edu.cibertec.apirestventascibertec.model.dto;
+
+import lombok.Data;
+
+@Data
+public class SupplierDto implements DtoEntity{
+
+    private Integer supplierid;
+    private String companyname;
+}
